cb3fd576bb96838684a5c92ee90a0bb20ca84662
Pablo <pabloariel413@gmail.com>
2023-04-14 10:23:47 -0300

    mocks

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,29 @@
+import {Task} from "./Task"
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: "Terminar primer modulo de Angular",
+        day: 'Abril 15 a las 12:00',
+        reminder: true
+    },
+    {
+        id:2,
+        text: "Hacer las compras para la cena",
+        day: 'Abril 15 a las 16:00',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "Investigar sobre Bootstrap",
+        day: 'Abril 16 a las 12:00',
+        reminder: false
+
+    },
+    {
+        id: 4,
+        text: "Leer El Alquimista",
+        day: 'Abril 13 a las 09:00',
+        reminder: false
+    }
+]
